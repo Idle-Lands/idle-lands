5003e314f33709f85f5383156e2247122a6654d6
MarcoWorms <marcogworms@gmail.com>
2018-06-08 01:41:04 -0300

    bots: add first example

---
+++ bots/simple-tree-cutting.js
@@ -0,0 +1,22 @@
+[
+  {
+    "type": "moveTo",
+    "payload": {
+      "x": 2,
+      "y": 8,
+    }
+  },
+  {
+    "type": "gatherResource"
+  },
+  {
+    "type": "moveTo",
+    "payload": {
+      "x": 2,
+      "y": 1
+    }
+  },
+  {
+    "type": "bankItems"
+  }
+]
